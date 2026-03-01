6c19f3f94d763d2e938f50d596261d407a4957dc
Sharada Mohanty <mohanty@aicrowd.com>
2026-03-01 21:00:27 +0530

    feat: add participant estimator sdk interfaces

--- src/circuit_estimation/__init__.py
+++ src/circuit_estimation/__init__.py
@@ -2,11 +2,14 @@
 
 from .domain import Circuit, Layer
 from .generation import random_circuit, random_gates
+from .sdk import BaseEstimator, SetupContext
 from .simulation import empirical_mean, run_batched, run_on_random
 
 __all__ = [
     "Circuit",
     "Layer",
+    "SetupContext",
+    "BaseEstimator",
     "random_circuit",
     "random_gates",
     "run_batched",
