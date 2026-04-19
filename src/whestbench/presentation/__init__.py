629e39453c3caf88e8d7ca544a6d4accb99c361f
Sharada Mohanty <mohanty@aicrowd.com>
2026-04-19 03:51:44 +0200

    refactor: add shared human rendering pipeline

--- src/whestbench/presentation/__init__.py
+++ src/whestbench/presentation/__init__.py
@@ -1,3 +1,21 @@
+from .blocks import (
+    build_budget_breakdown_block as build_budget_breakdown_block,
+)
+from .blocks import (
+    build_score_block as build_score_block,
+)
+from .blocks import (
+    make_keyed_label as make_keyed_label,
+)
+from .human import (
+    create_console as create_console,
+)
+from .human import (
+    render_blocks as render_blocks,
+)
+from .human import (
+    render_document as render_document,
+)
 from .models import CommandPresentation as CommandPresentation
 from .render_plain import render_plain_presentation as render_plain_presentation
 from .render_rich import render_rich_presentation as render_rich_presentation
